d881df23fc599dcb0d2816e9ce2f4f8c16bac8e9
Modello1911 <guillermo.escobar11@yahoo.com>
2024-03-16 14:46:16 -0700

    new 2

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,12 +1,16 @@
 import "./Product.css";
+import QuantityPicker from './QuantityPicker';
 function Product(props) {
     return (
         <div className="product">
             <img src="https://picsum.photos/200" />
             <h5>{props.info.title}</h5>
             <label>${props.info.price}</label>
+
+            <QuantityPicker />
         </div>
     );
 }
 
 export default Product;
+
