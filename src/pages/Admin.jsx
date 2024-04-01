4f2eaa6fd7615737720a8cae131af829706bc61b
Modello1911 <guillermo.escobar11@yahoo.com>
2024-04-01 16:02:21 -0700

    new but needs fixing

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,17 +1,141 @@
-import './Admin.css';
+import { useState } from "react";
+import "./Admin.css";
 
 function Admin() {
+  const [coupon, setCoupon] = useState({
+    code: "",
+    discount: "",
+  });
+  const [allCoupons, setAllCoupons] = useState([]);
+
+  function handleCouponChange(e) {
+    const val = e.target.value;
+    const name = e.target.name;
+
+    var copy = { ...coupon };
+    copy[name] = val;
+    setCoupon(copy);
+  }
+
+  function saveCoupon() {
+    console.log(coupon);
+
+    let copy = [...allCoupons];
+    copy.push(coupon);
+    setAllCoupons(copy);
+  }
+  /** */
+
+  const [product, setProduct] = useState({
+    title: "",
+    catagory: "",
+    image: "",
+    price: "",
+  });
+  const [allProducts, setAllProducts] = useState([]);
+
+  function handleProductChange(e) {
+    const val = e.target.value;
+    const name = e.target.name;
+    var copy = { ...product };
+    copy[name] = val;
+    setProduct(copy);
+  }
+  function saveProduct() {
+    console.log(product);
+
+    var copy = [...allProducts];
+    copy.push(product);
+    setAllProducts(copy);
+  }
+
   return (
-    <div className="admin page">
+    <div className="parent">
       <h1>Store Administration</h1>
-
-      <div className="parent">
-        <div className="prods">
+      <div className="prods">
+        <div className="col-sm-4">
           <h3>Products</h3>
+          <div className="form-group">
+            <label className="form-label">Title</label>
+            <input
+              name="title"
+              type="text"
+              className="form-control"
+              onChange={handleProductChange}
+            />
+          </div>
+          <div>
+            <label className="form-label">Category</label>
+            <input
+              name="category"
+              type="text"
+              className="form-control"
+              onChange={handleProductChange}
+            />
+          </div>
+          <div>
+            <label className="form-label">Image</label>
+            <input
+              name="image"
+              type="text"
+              className="form-control"
+              onChange={handleProductChange}
+            />
+          </div>
+          <div>
+            <label className="form-label">Price</label>
+            <input
+              name="price"
+              type="number"
+              className="form-control"
+              onChange={handleProductChange}
+            />
+          </div>
+          <div className="form-group">
+            <button onClick={saveProduct} className="btn btn-primary">
+              Save Products
+            </button>
+          </div>
+          <ul>
+            {allProducts.map((p, index) => (
+              <li key={index}>
+                {p.title} - {p.category} - ${p.price}
+              </li>
+            ))}
+          </ul>
         </div>
-
-        <div className="coupons">
+        <div className="col-sm-3">
           <h3>Coupons</h3>
+          <div className="form-group">
+            <label className="form-label">Code</label>
+            <input
+              name="code"
+              type="text"
+              className="form-control"
+              onChange={handleCouponChange}
+            />
+          </div>
+          <div className="col-sm-12">
+            <label className="form-label">Discount</label>
+            <input
+              name="discount"
+              type="number"
+              className="form-control"
+              onChange={handleCouponChange}
+            />
+          </div>
+          <div className="form-group">
+            <button onClick={saveCoupon} className="btn btn-primary">
+              Save Coupon
+            </button>
+          </div>
+          <ul>
+            {allCoupons.map((c, index) => (
+              <li key={index}>
+                {c.code} - ${c.discount}
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,16 @@
-import './Home.css';
+import { Link } from "react-router-dom";
+import "./Home.css";
 
 function Home() {
-    return (
-        <div className='Home page'>
-            <h1>Welcome to Plants Supreme</h1>
-        </div>
-    )
+  return (
+    <div className="Home page">
+      <h1>Welcome to Plants Supreme</h1>
+
+      <Link className="btn btn-primary" to="/catalog">
+        Check out our catalog
+      </Link>
+    </div>
+  );
 }
 
 export default Home;
